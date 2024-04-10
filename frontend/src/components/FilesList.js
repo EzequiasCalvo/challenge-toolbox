427a3bc79fb834bd1f9a468cad743a3d59cd2924
EzequiasCalvo <calvo.ezequias@gmail.com>
2024-04-09 22:27:42 -0300

    feat: filter by file name

--- frontend/src/components/FilesList.js
+++ frontend/src/components/FilesList.js
@@ -1,40 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Table } from "react-bootstrap";
+import { Table, Form, Button, Row, Col } from "react-bootstrap";
 import { fetchFiles } from "../redux/actions/fileActions";
 
 const FilesList = () => {
   const dispatch = useDispatch();
   const files = useSelector((state) => state.file.files);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     dispatch(fetchFiles());
   }, [dispatch]);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch(fetchFiles(filter));
+  };
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>File Name</th>
-          <th>Text</th>
-          <th>Number</th>
-          <th>Hex</th>
-        </tr>
-      </thead>
-      <tbody>
-        {files.map(({ file, lines }, fileIndex) => {
-          if (lines.length === 0) return null;
-          return lines.map(({ text, number, hex }, lineIndex) => (
-            <tr key={`${fileIndex}-${lineIndex}`}>
-              <td>{file}</td>
-              <td>{text}</td>
-              <td>{number}</td>
-              <td>{hex}</td>
+    <>
+      <Form>
+        <Form.Group controlId="fileNameFilter">
+          <Row className="mt-4 mb-2">
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Filter by file name"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+              />
+            </Col>
+            <Col>
+              <Button variant="primary" type="submit" onClick={handleSearch}>
+                Buscar
+              </Button>
+            </Col>
+          </Row>
+        </Form.Group>
+      </Form>
+      {files.length ? (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>File Name</th>
+              <th>Text</th>
+              <th>Number</th>
+              <th>Hex</th>
             </tr>
-          ));
-        })}
-      </tbody>
-    </Table>
+          </thead>
+          <tbody>
+            {files.map(({ file, lines }, fileIndex) => {
+              if (lines.length === 0) return null;
+              return lines.map(({ text, number, hex }, lineIndex) => (
+                <tr key={`${fileIndex}-${lineIndex}`}>
+                  <td>{file}</td>
+                  <td>{text}</td>
+                  <td>{number}</td>
+                  <td>{hex}</td>
+                </tr>
+              ));
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        <p>No files found</p>
+      )}
+    </>
   );
 };
 
--- frontend/src/redux/actions/fileActions.js
+++ frontend/src/redux/actions/fileActions.js
@@ -1,12 +1,19 @@
-export const fetchFiles = () => async (dispatch) => {
-  try {
-    const response = await fetch("http://localhost:4000/files/data");
-    const data = await response.json();
-    dispatch({
-      type: "FETCH_FILES_SUCCESS",
-      payload: data,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+export const fetchFiles =
+  (fileName = "") =>
+  async (dispatch) => {
+    let url = "http://localhost:4000/files/data";
+    if (fileName) {
+      url += `?fileName=${encodeURIComponent(fileName)}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: "FETCH_FILES_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
