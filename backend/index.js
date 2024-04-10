73daf430cdfe547994578e029755146693b33586
EzequiasCalvo <calvo.ezequias@gmail.com>
2024-04-09 23:30:16 -0300

    feat: add docker to bk

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,29 @@
-import express from "express";
-import filesRouter from "./routes/filesRouter.js";
+import express from 'express'
+import filesRouter from './routes/filesRouter.js'
 
-const app = express();
-const PORT = 4000;
+const app = express()
+const PORT = 4000
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
 
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
 
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
   }
 
-  next();
-});
+  next()
+})
 
-app.use("/files", filesRouter);
+app.use('/files', filesRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
 
-export const server = app;
+export const server = app
